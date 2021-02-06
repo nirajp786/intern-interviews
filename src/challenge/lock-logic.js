dae00fa3d164d9b8f30f50a809a6abfd912ece22
Niraj Patel <786nirajpatel@gmail.com>
2021-02-06 18:33:36 -0500

    Finished the lock logic and About me section

--- src/challenge/lock-logic.js
+++ src/challenge/lock-logic.js
@@ -16,15 +16,39 @@ function changeDialValue (index, incrementBy) {
   // for example, if the user clicked the "down" arrow for the last wheel
   // this will be called with arguments (3, -1).
 
+  
+  
   // to change the state of the lock, simply make a call like
   // lockState.locked = false
   // or lockState.wheels[1] = 2
   // the lock will re-render itself when the value changes
-
+  
+  if(incrementBy == 1){
+    if(lockState.wheels[index] < 9){
+      lockState.wheels[index] = lockState.wheels[index] + 1;
+    }
+    else if(lockState.wheels[index] == 9){
+      lockState.wheels[index] = 0
+    }
+  }
+  else if(incrementBy == -1){
+    if(lockState.wheels[index] > 0){
+      lockState.wheels[index] = lockState.wheels[index] - 1;
+    }
+    else if(lockState.wheels[index] == 0){
+      lockState.wheels[index] = 9
+    }
+  }
   // When the lock is set to match the secretCombo
   // call the redirect() function with your name
   // eg: redirect('larry-lobster')
   // the redirect function will only redirect if the lockState is unlocked
+
+  if(lockState.wheels[0] == SECRET_COMBO[0] && lockState.wheels[1] == SECRET_COMBO[1] && lockState.wheels[2] == SECRET_COMBO[2] &&
+    lockState.wheels[3] == SECRET_COMBO[3]){
+      lockState.locked = false;
+      redirect('Niraj-Patel')
+    }
 }
 
 // let our other modules find our functions
